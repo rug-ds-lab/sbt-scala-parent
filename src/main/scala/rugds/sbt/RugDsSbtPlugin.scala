18d54807e31a2281f6146c46b04282fdbd08463b
lazovik <lazovik@gmail.com>
2014-08-08 18:10:52 +0200

    added support for spray-based projects

--- src/main/scala/rugds/sbt/RugDsSbtPlugin.scala
+++ src/main/scala/rugds/sbt/RugDsSbtPlugin.scala
@@ -22,24 +22,24 @@ trait Dependencies {
 
 
   // libraries
-  val slf4j       = "org.slf4j" % "slf4j-api"    % slf4jV
-  val jodaTime    = "joda-time" % "joda-time"    % jodaTimeV
-  val jodaConvert = "org.joda"  % "joda-convert" % jodaConvertV
+  val slf4j       = "org.slf4j" % "slf4j-api"    % slf4jV       withSources() withJavadoc()
+  val jodaTime    = "joda-time" % "joda-time"    % jodaTimeV    withSources() withJavadoc()
+  val jodaConvert = "org.joda"  % "joda-convert" % jodaConvertV withSources() withJavadoc()
 
-  val logbackClassic = "ch.qos.logback" % "logback-classic"  % logbackV
-  val logbackCore    = "ch.qos.logback" % "logback-core"     % logbackV
-  val jclOverSlf4j   = "org.slf4j"      % "jcl-over-slf4j"   % slf4jV
-  val log4jOverSlf4j = "org.slf4j"      % "log4j-over-slf4j" % slf4jV
+  val logbackClassic = "ch.qos.logback" % "logback-classic"  % logbackV withSources() withJavadoc()
+  val logbackCore    = "ch.qos.logback" % "logback-core"     % logbackV withSources() withJavadoc()
+  val jclOverSlf4j   = "org.slf4j"      % "jcl-over-slf4j"   % slf4jV   withSources() withJavadoc()
+  val log4jOverSlf4j = "org.slf4j"      % "log4j-over-slf4j" % slf4jV   withSources() withJavadoc()
 
-  val junit = "junit" % "junit" % junitV % Test
+  val junit = "junit" % "junit" % junitV % Test withSources() withJavadoc()
 
-  val typesafeConfig = "com.typesafe" %  "config"         % typesafeConfigV
-  val grizzledLog    = "org.clapper"  %% "grizzled-slf4j" % grizzledLogV
-  val scalaz         = "org.scalaz"   %% "scalaz-core"    % scalazV
+  val typesafeConfig = "com.typesafe" %  "config"         % typesafeConfigV withSources() withJavadoc()
+  val grizzledLog    = "org.clapper"  %% "grizzled-slf4j" % grizzledLogV    withSources() withJavadoc()
+  val scalaz         = "org.scalaz"   %% "scalaz-core"    % scalazV         withSources() withJavadoc()
 
-  val specs      = "org.specs2"     %% "specs2"     % specsV      % Test
-  val scalaTest  = "org.scalatest"  %% "scalatest"  % scalaTestV  % Test
-  val scalaCheck = "org.scalacheck" %% "scalacheck" % scalaCheckV % Test
+  val specs      = "org.specs2"     %% "specs2"     % specsV      % Test withSources() withJavadoc()
+  val scalaTest  = "org.scalatest"  %% "scalatest"  % scalaTestV  % Test withSources() withJavadoc()
+  val scalaCheck = "org.scalacheck" %% "scalacheck" % scalaCheckV % Test withSources() withJavadoc()
 
   // groups of libraries
   val logViaLog4j         = Seq(logbackClassic, logbackCore, jclOverSlf4j, log4jOverSlf4j)
@@ -47,6 +47,39 @@ trait Dependencies {
   val javaOnly            = Seq(slf4j, jodaTime, jodaConvert, junit)
   val scalaBasic          = javaOnly ++ Seq(typesafeConfig, grizzledLog, scalaz, specs, scalaTest, scalaCheck)
 }
+
+
+trait SprayAkkaDependencies {
+  this: Dependencies =>
+
+  // versions
+  val sprayV      = "1.3.1"
+  val sprayJsonV  = "1.2.6"
+  val akkaV       = "2.3.4"
+
+  // libraries
+  val sprayClient  = "io.spray"          %% "spray-client"      % sprayV      withSources() withJavadoc()
+  val sprayCan     = "io.spray"          %% "spray-can"         % sprayV      withSources() withJavadoc()
+  val sprayRouting = "io.spray"          %% "spray-routing"     % sprayV      withSources() withJavadoc()
+  val sprayHttpx   = "io.spray"          %% "spray-httpx"       % sprayV      withSources() withJavadoc()
+  val sprayIO      = "io.spray"          %% "spray-io"          % sprayV      withSources() withJavadoc()
+  val sprayJson    = "io.spray"          %% "spray-json"        % sprayJsonV  withSources() withJavadoc()
+  val akkaActor    = "com.typesafe.akka" %% "akka-actor"        % akkaV       withSources() withJavadoc()
+  val akkaSlf4j    = "com.typesafe.akka" %% "akka-slf4j"        % akkaV       withSources() withJavadoc()
+
+  // repositories
+  val sprayRepo = "spray.io" at "http://repo.spray.io"
+
+  // aggregated dependency
+  val akkaDependencies  = scalaBasic       ++ Seq(akkaActor, akkaSlf4j)
+  val sprayDependencies = akkaDependencies ++ Seq(sprayCan, sprayClient, sprayRouting, sprayHttpx, sprayIO, sprayJson)
+}
+
+trait NotUsedDependencies {
+  val amqpClientV  = "1.4"
+  val amqpClient   = "com.github.sstone" %% "amqp-client" % amqpClientV withSources() withJavadoc()
+}
+
 
 trait CommonSettings {
   val nexus     = "http://sm4all-project.eu/nexus"
@@ -64,35 +97,29 @@ trait CommonSettings {
 
   val commonSettings = Seq(
     organization := "rugds",
+    scalaVersion := scalaV,
     publishSetting,
     publishArtifact in (Compile, packageSrc) := false, // disable publishing the main sources jar
     publishArtifact in (Compile, packageDoc) := false, // disable publishing the main API jar
     credentials += Credentials(Path.userHome / ".ivy2" / ".credentials"),
     resolvers ++= repositories
   )
-
-  // check the latest versions of all dependencies (wrapped around sbt-updates plugin)
-//  val dependencyUpdates    = TaskKey[Unit]("dependencyUpdates")
-//  val dependencyUpdatesDef = dependencyUpdates <<= (dependencyUpdates.asInstanceOf[TaskKey[_]]) map (_) // we just reuse existing plugin functionality
-//  val rugdsConfig = config("rugds") // separate namespace for custom commands
 }
 
-trait Projects extends Dependencies with CommonSettings {
+trait Projects extends Dependencies with CommonSettings with SprayAkkaDependencies with NotUsedDependencies {
   private def logDependency(includeLog: Boolean) = (if (includeLog) logViaLog4j else logViaLog4jTestOnly)
 
-  def javaProject(name: String, basedir: String = ".", includeLog: Boolean = false) = {
-    val dependencies = javaOnly ++ logDependency(includeLog)
+  private def genericProject(name: String, basedir: String, dependencies: Seq[ModuleID], includeLog: Boolean) = {
+    val finalDependencies = dependencies ++ logDependency(includeLog)
     Project(name, file(basedir), settings = commonSettings ++ Seq(
       libraryDependencies ++= dependencies
     ))
   }
 
-  def scalaProject(name: String, basedir: String = ".", includeLog: Boolean = false) = {
-    val dependencies = scalaBasic ++ logDependency(includeLog)
-    Project(name, file(basedir), settings = commonSettings ++ Seq(
-      scalaVersion := scalaV,
-      libraryDependencies ++= dependencies
-    ) /* ++ inConfig(rugdsConfig)(Seq(dependencyUpdates)) */)
+  def javaProject (name: String, basedir: String = ".", includeLog: Boolean = false) = genericProject(name, basedir, javaOnly,          includeLog)
+  def scalaProject(name: String, basedir: String = ".", includeLog: Boolean = false) = genericProject(name, basedir, scalaBasic,        includeLog)
+  def sprayProject(name: String, basedir: String = ".", includeLog: Boolean = false) = genericProject(name, basedir, sprayDependencies, includeLog) settings {
+    resolvers += sprayRepo
   }
 }
 
