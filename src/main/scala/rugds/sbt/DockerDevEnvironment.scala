f82a958197c2937210c8308d50376c8a377e0e06
lazovik <lazovik@gmail.com>
2016-11-04 16:52:22 +0100

    fixing formatting

--- src/main/scala/rugds/sbt/DockerDevEnvironment.scala
+++ src/main/scala/rugds/sbt/DockerDevEnvironment.scala
@@ -31,7 +31,8 @@ trait DockerDevEnvironment {
       val projectId = toEnvModule(CrossVersion(sV, sbV)(projectID.value))
       val jarDependencies = mg.dependencyMap.toList.filter {
         case (module, dependencies) => mg.module(module).isUsed // remove evicted modules
-      }.map {
+      }
+      .map {
         case (module, dependencies) => {
           val envModule = EnvModule(module.name, module.organisation, module.version, mg.module(module).jarFile.map(x => new JarFile(x)))
           envModule -> dependencies
@@ -40,35 +41,34 @@ trait DockerDevEnvironment {
             .toSet
         }
       }
-        .filter {
-          // remove dependencies that do not have docker (module)
-          case (module, dependencies) => module.jarFile.map(containsDockerDependency).getOrElse(true)
-        }
-        .map {
-          // remove dependencies that do not have docker (dependencies)
-          case (module, dependencies) => module -> dependencies.filter(_.jarFile.map(containsDockerDependency).getOrElse(true))
-        }
-        .map {
-          // replace project dependencies (with no jar) with its jar equivalents
-          case (module, dependencies) =>
-            val m = projectJarDependencies.find(_ == module).getOrElse(module)
-            val d = dependencies.map(dm => projectJarDependencies.find(_ == dm).getOrElse(dm))
-            m -> d
-        }
-        .filter {
-          // remove projects without jar
-          case (module, dependencies) => module.jarFile.isDefined
-        }
-        .map {
-          // remove projects without jar in dependencies
-          case (module, dependencies) => module -> dependencies.filter(_.jarFile.isDefined)
-        }
-      jarDependencies.map {
-        case (module, dependencies) => if (module == projectId) module -> ((jarDependencies.toMap.keySet ++ dependencies) - module) else module -> dependencies
-      }.sortWith {
-        case ((m1, d1), (m2, d2)) => d2.contains(m1) || !d1.contains(m2)
+      .filter {
+        // remove dependencies that do not have docker (module)
+        case (module, dependencies) => module.jarFile.map(containsDockerDependency).getOrElse(true)
       }
-    },
+      .map {
+        // remove dependencies that do not have docker (dependencies)
+        case (module, dependencies) => module -> dependencies.filter(_.jarFile.map(containsDockerDependency).getOrElse(true))
+      }
+      .map {
+        // replace project dependencies (with no jar) with its jar equivalents
+        case (module, dependencies) =>
+          val m = projectJarDependencies.find(_ == module).getOrElse(module)
+          val d = dependencies.map(dm => projectJarDependencies.find(_ == dm).getOrElse(dm))
+          m -> d
+      }
+      .filter {
+        // remove projects without jar
+        case (module, dependencies) => module.jarFile.isDefined
+      }
+      .map {
+        // remove projects without jar in dependencies
+        case (module, dependencies) => module -> dependencies.filter(_.jarFile.isDefined)
+      }
+    jarDependencies.map {
+      case (module, dependencies) => if (module == projectId) module -> ((jarDependencies.toMap.keySet ++ dependencies) - module) else module -> dependencies
+    }.sortWith {
+      case ((m1, d1), (m2, d2)) => d2.contains(m1) || !d1.contains(m2)
+    }},
 
     envDockerDependencyTree := envProjectDependencyTree.value.flatMap {
       case (module, dependencies) => module.toEnvDocker.map(_ -> dependencies.flatMap(_.toEnvDocker))
@@ -100,7 +100,7 @@ trait DockerDevEnvironment {
         case (docker, dependencies) =>
           val dockerPsCmd = Seq("docker", "ps", "-aq", "--format", "{{ .Names}}"); log.info(dockerPsCmd.mkString(" "))
           if (!dockerPsCmd.lines.contains(docker.name)) { // if it does not exist => run it
-          val dockerInspectCmd = Seq("docker", "inspect", "-f", "{{range $key, $item := .Config.ExposedPorts}}{{$key}} {{end}}", docker.toString); log.info(dockerInspectCmd.mkString(" "))
+            val dockerInspectCmd = Seq("docker", "inspect", "-f", "{{range $key, $item := .Config.ExposedPorts}}{{$key}} {{end}}", docker.toString); log.info(dockerInspectCmd.mkString(" "))
             val dockerExposedPorts = dockerInspectCmd.!!
             val exposedPorts = dockerExposedPorts.trim.split(' ').map(port => port.split('/')).map(list => (list.head, list.last)).map {
               case (port, _) => s"-p $port:$port"
